fd531cd73bac195f88e17c91dc32544dea3b71f4
NITRO5\daniel <daniel@softinline.com>
2023-12-13 10:22:16 +0100

    move route to middleware web

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,4 +2,6 @@
 
     use Illuminate\Support\Facades\Route;
 
-    Route::post('jcrud/datatable/config-columns-save', 'Softinline\JCrud\Controllers\DatatableController@configColumnsSave');
+    Route::middleware(['web'])->group(function () {
+        Route::post('jcrud/datatable/config-columns-save', 'Softinline\JCrud\Controllers\DatatableController@configColumnsSave');
+    })
